3025ad8a950469566a5c46e2367c04f00f6b16d4
NattawutR <nattawutroj@gmail.com>
2024-10-26 14:04:38 +0700

    Refactor: Introduce Base layout component
    Adds a Base layout component to wrap the application's content within the RootLayout. This improves code structure and allows for consistent application of shared layout elements.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 p-4 pt-2 sm:items-start">
+      <main className="row-start-2gap-8 flex flex-col items-center p-4 pt-2 sm:items-start">
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
